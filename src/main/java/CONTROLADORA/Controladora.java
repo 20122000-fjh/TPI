18d7c7e845b000f2e5f7d062e8e2d72565547ec8
FJHILLEBRAND <71841310+20122000-fjh@users.noreply.github.com>
2024-01-26 14:03:27 -0300

    Add files via upload

--- src/main/java/CONTROLADORA/Controladora.java
+++ src/main/java/CONTROLADORA/Controladora.java
@@ -1,6 +1,5 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * aaaaaaa
  */
 package CONTROLADORA;
 import java.awt.event.ActionEvent;
